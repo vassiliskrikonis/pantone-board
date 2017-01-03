3d3b91c2a6086220898fead33c91c51c960bfcac
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2017-01-03 21:31:08 +0200

    added material-ui and floating action button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import Post from './Post';
 import './App.css';
 
@@ -17,10 +21,16 @@ class App extends Component {
 
   render() {
     const colors = this.state.colors.map( ({hex, name}) => <Post key={hex} hex={hex} text={name} /> );
+    injectTapEventPlugin();
     return (
+      <MuiThemeProvider>
       <div className="App board">
         {colors}
+        <FloatingActionButton style={{position: 'absolute', bottom: 20, right: 20}}>
+          <ContentAdd />
+        </FloatingActionButton>
       </div>
+      </MuiThemeProvider>
     );
   }
 }
