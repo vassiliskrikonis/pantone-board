e9800f1d075d06038e1e341c7230c8846150537c
Vassilis Krikonis <vasiliskrikonis@gmail.com>
2017-06-20 13:53:40 +0300

    color array index as key

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import Color from './Color'
 
+/**
+ * A Board component displays Color components
+ * Index in array is used as key while iterating through colors since they won't rearrange anytime.
+ */
 const Board = ({colors}) => (
   <div className="Board">
-    { colors.map( (color, i) => <Color hex={color} /> ) }
+    { colors.map( (color, i) => <Color key={i} hex={color} /> ) }
   </div>
 )
 
